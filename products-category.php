4c73d77d473e1f03a788e4c2385eb8634dd1a0fe
Jarod Bernabe <jarodbernabe1100@gmail.com>
2022-04-21 18:27:41 +0800

    changed logo

--- products-category.php
+++ products-category.php
@@ -12,7 +12,8 @@
         <div class="sidebar-header">
           <div class="d-flex justify-content-between">
             <div class="logo">
-              <a href="index.php"><img src="assets/images/logo/logo.png" alt="Logo" srcset="" /></a>
+              <!-- <a href="index.php"><img src="assets/images/logo/logo.png" alt="Logo" srcset="" /></a> -->
+              <a href="index.php">Vape Shop</a>
             </div>
             <div class="toggler">
               <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
